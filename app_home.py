b79e7d2f531aa078c732793875ea0e00541b1246
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-30 18:10:37 +0900

    add

--- app_home.py
+++ app_home.py
@@ -5,8 +5,9 @@ def run_home() :
     df1 = pd.read_csv('data/pre_data.csv', index_col=0)
     df2 = pd.read_csv('data/X.csv', index_col=0)
     search = st.text_input('웹툰명이나 장르 검색')
-    df1 = pd.read_csv('data/pre_data.csv', index_col=0)
+    
+    df1['title'].str.lower().str.contains(search)
     st.dataframe(df1)
 
-    df2 = pd.read_csv('data/X.csv', index_col=0)
+    
     st.dataframe(df2)
