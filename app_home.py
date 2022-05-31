a920f02b885ce6d99ecdc56d19a9fafb96aae947
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-31 09:49:58 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -4,10 +4,16 @@ import pandas as pd
 def run_home() :
     df1 = pd.read_csv('data/pre_data.csv', index_col=0)
     df2 = pd.read_csv('data/X.csv', index_col=0)
+    
     search = st.text_input('웹툰명이나 장르 검색')
-    
-    df1['title'].str.lower().str.contains(search)
-    st.dataframe(df1)
+    search_result = df1.loc[df1['title'].str.lower().str.contains(search) | 
+            df1['genre'].str.lower().str.contains(search),]
 
-    
-    st.dataframe(df2)
+
+    search_list = search_result['title'].values
+    if len(search_list) != 0 :
+        selected = st.selectbox('검색 결과 리스트', search_list)
+        st.subheader(selected)
+    else :
+        st.text('검색 결과가 없습니다.')
+
